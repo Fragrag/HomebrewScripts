0e35f2b559e06a3b5019f83528f00c81a9c43498
Fragrag <haryo.sukmawanto@gmail.com>
2018-01-27 15:27:14 +0100

    Added start of VideoMetaData Powershell script which will look up metadata of videofiles

---
+++ VideoMetaData/VideoMetaData.ps1
@@ -0,0 +1,5 @@
+﻿# https://blogs.technet.microsoft.com/heyscriptingguy/2014/02/06/use-powershell-to-find-metadata-from-photograph-files/
+
+
+$videoMetadata = Get-FileMetaData -folder (Get-childitem \\haryonas\media\video -Recurse -Directory).FullName
+
