e6d7e679e02a416ec6d4bc4f2e4f7e48d8e52917
Fragrag <haryo.sukmawanto@gmail.com>
2018-02-06 22:20:06 +0100

    -VideoMetaData.py iterates through subfolders correctly

--- VideoMetaData/VideoMetaData.py
+++ VideoMetaData/VideoMetaData.py
@@ -4,7 +4,9 @@ import os
 # Initialising variables
 ItemExtensions = '.mp4', '.avi', '.mkv' # List of possible video formats 
 ScriptPath = os.path.dirname(os.path.realpath(__file__)) # Current location of script
-TargetDirectory = ScriptPath + '\Videos' # Designates directory to scan. Needs to be changed for production
+#TargetDirectory = ScriptPath + '\Videos' # Designates directory to scan. Needs to be changed for production
+TargetDirectory = r'\\HARYONAS\media\video\TV'
+TargetDirectoryfsencode = os.fsencode(TargetDirectory)
 ffprobeArguments = " -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 " # FFProbe arguments to output only the codec
 
 # Initialises and creates a new .csv, setting up basic parameters to work with it
@@ -14,20 +16,21 @@ with open('output.csv', 'w') as csvfile:
 	csvOutput.writeheader()
 
 	# Go through every file in folder
-	for item in os.listdir(TargetDirectory): 
-		if item.endswith(ItemExtensions):
-			locationItem = os.path.join(TargetDirectory, item)
-			returnedCodec = os.popen("ffprobe" + ffprobeArguments + "\"" + os.path.join(TargetDirectory, item)).read()
+	for root, dirs, files in os.walk(TargetDirectory):
+		for item in files:
+			if item.endswith(ItemExtensions):
+				locationItem = os.path.join(root, item) 
+				returnedCodec = os.popen("ffprobe" + ffprobeArguments + '\"' + locationItem + '\"').read()
 		
-			#print("\n")
-			print(locationItem)
-			print(returnedCodec)
+				#print("\n")
+				print(locationItem)
+				print(returnedCodec)
 			
-			csvOutput.writerow({'Filename': item, 'Item location': locationItem, 'Codec': returnedCodec}) 
+				csvOutput.writerow({'Filename': item, 'Item location': locationItem, 'Codec': returnedCodec}) 
 		
-		else: # If file does not end in a video format, throw error and does not add it to the list.
-			print("Error!")
-			print(os.path.join(TargetDirectory, item) + " does not end in " + ', '.join(ItemExtensions))
-			print("\n")
+			else: # If file does not end in a video format, throw error and does not add it to the list.
+				print("Error!")
+				print(os.path.join(r, item) + " does not end in " + ', '.join(ItemExtensions))
+				print("\n")
 		
 		
