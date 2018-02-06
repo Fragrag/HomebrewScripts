82b7bea7cc5ce4e67655c0ab396443132be75eee
Fragrag <haryo.sukmawanto@gmail.com>
2018-02-06 23:10:49 +0100

    -Cleaned up VideoMetaData.py, much slimmer and cleaner now

--- VideoMetaData/VideoMetaData.py
+++ VideoMetaData/VideoMetaData.py
@@ -1,13 +1,18 @@
+##########################################################################
+#                                                                        #
+# VideoMetaData.py                                                       #
+#                                                                        #
+# This script goes through files in a folder and its subfolders          #
+# to output file location and codec type to output.csv                   #      
+#                                                                        #
+##########################################################################
+
 import csv
 import os
 
-# Initialising variables
-ItemExtensions = '.mp4', '.avi', '.mkv' # List of possible video formats 
-ScriptPath = os.path.dirname(os.path.realpath(__file__)) # Current location of script
-#TargetDirectory = ScriptPath + '\Videos' # Designates directory to scan. Needs to be changed for production
+# Initialising start variables
 TargetDirectory = r'\\HARYONAS\media\video'
-TargetDirectoryfsencode = os.fsencode(TargetDirectory)
-ffprobeArguments = " -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 " # FFProbe arguments to output only the codec
+ffprobeArguments = " -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "
 
 # Initialises and creates a new .csv, setting up basic parameters to work with it
 with open('output.csv', 'w') as csvfile: 
@@ -15,22 +20,13 @@ with open('output.csv', 'w') as csvfile:
 	csvOutput = csv.DictWriter(csvfile, fieldnames=header, lineterminator='\n', delimiter=',')
 	csvOutput.writeheader()
 
-	# Go through every file in folder
+	# Go through every file in folder and subfolder
 	for root, dirs, files in os.walk(TargetDirectory):
 		for item in files:
-			if item.endswith(ItemExtensions):
-				locationItem = os.path.join(root, item) 
-				returnedCodec = os.popen("ffprobe" + ffprobeArguments + '\"' + locationItem + '\"').read()
+			locationItem = os.path.join(root, item) 
+			returnedCodec = os.popen("ffprobe" + ffprobeArguments + '\"' + locationItem + '\"').read()
 		
-				#print("\n")
-				print(locationItem)
-				print(returnedCodec)
+			print(locationItem)
+			print(returnedCodec)
 			
-				csvOutput.writerow({'Filename': item, 'Item location': locationItem, 'Codec': returnedCodec}) 
-		
-			else: # If file does not end in a video format, throw error and does not add it to the list.
-				print("Error!")
-				print(os.path.join(root, item) + " does not end in " + ', '.join(ItemExtensions))
-				print("\n")
-		
-		
+			csvOutput.writerow({'Filename': item, 'Item location': locationItem, 'Codec': returnedCodec}) 
