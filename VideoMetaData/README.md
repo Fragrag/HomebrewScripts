4913c4d2299e52274e5030ce3e154e3c648ca09c
Fragrag <haryo.sukmawanto@gmail.com>
2018-02-06 17:48:26 +0100

    - Feature complete VideoMetaData.py to output file, location and codec to a .csv

---
+++ VideoMetaData/README.md
@@ -0,0 +1,7 @@
+VideoMetaData.py
+A script that will go through files and output a csv with 
+file, file location and codec
+
+Dependencies:
+Python
+ffmpeg
--- VideoMetaData/VideoMetaData.py
+++ VideoMetaData/VideoMetaData.py
@@ -1,13 +1,33 @@
-import csv, os
+import csv
+import os
 
-ItemExtensions = '.mp4', '.avi', '.mkv'
-TargetDirectory = 'D:\Videos'
+# Initialising variables
+ItemExtensions = '.mp4', '.avi', '.mkv' # List of possible video formats 
+ScriptPath = os.path.dirname(os.path.realpath(__file__)) # Current location of script
+TargetDirectory = ScriptPath + '\Videos' # Designates directory to scan. Needs to be changed for production
+ffprobeArguments = " -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 " # FFProbe arguments to output only the codec
 
-for item in os.listdir(TargetDirectory):	
-	if item.endswith(ItemExtensions):
-		itemLocation = os.path.join(TargetDirectory, item)
-		print(os.path.join(TargetDirectory, item))
+# Initialises and creates a new .csv, setting up basic parameters to work with it
+with open('output.csv', 'w') as csvfile: 
+	header = ['Filename', 'Item location', 'Codec']
+	csvOutput = csv.DictWriter(csvfile, fieldnames=header, lineterminator='\n', delimiter=',')
+	csvOutput.writeheader()
+
+	# Go through every file in folder
+	for item in os.listdir(TargetDirectory): 
+		if item.endswith(ItemExtensions):
+			locationItem = os.path.join(TargetDirectory, item)
+			returnedCodec = os.popen("ffprobe" + ffprobeArguments + "\"" + os.path.join(TargetDirectory, item)).read()
 		
-		os.system("ffmpeg -i \"" + os.path.join(TargetDirectory, item) + "\" -f ffmetadata metadata.txt")
-	else:
-		print("does not end in " + itemExtensions)
+			#print("\n")
+			print(locationItem)
+			print(returnedCodec)
+			
+			csvOutput.writerow({'Filename': item, 'Item location': locationItem, 'Codec': returnedCodec}) 
+		
+		else: # If file does not end in a video format, throw error and does not add it to the list.
+			print("Error!")
+			print(os.path.join(TargetDirectory, item) + " does not end in " + ', '.join(ItemExtensions))
+			print("\n")
+		
+		
