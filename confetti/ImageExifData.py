c1a0195524979df38decad51712abdabc9beb408
Fragrag <haryo.sukmawanto@gmail.com>
2018-02-12 23:42:26 +0100

    - Renamed script to ImageExifData.py - Further work on ImageExifData.py - Changed ApertureValue exif tag from `0x9202 ApertureValue` to `0x829d fnumber`

--- confetti/ImageExifData.py
+++ confetti/ImageExifData.py
@@ -5,34 +5,51 @@
 # Focal length = 0x920a
 # Aperture = 0x9202
 
+# Dependencies
+# csv, os, Pillow, sys
+
 import csv
 import os
 import sys
-from PIL import Image
-from PIL.ExifTags import TAGS
+import PIL.Image
 
-TargetDirectory = sys.argv[1]
+try:
+	TargetDirectory = sys.argv[1]
+	assert TargetDirectory
+except:
+	print("Please specify a folder")
+	sys.exit(1)
+	
+AcceptedFiles = '.psd', '.cr2', '.jpg', '.jpeg', '.dng'
 
-# Pseudocode:
 
 with open('output.csv', 'w') as csvfile: 		# Initialize and create new .csv
-	# Define and create headers
 	header = ['Filename', 'Camera Model', 'Focal length', 'Aperture size']
 	csvOutput = csv.DictWriter(csvfile, fieldnames=header, lineterminator='\n', delimiter=',')
 	csvOutput.writeheader()
+	print("")
 	
 	for item in os.listdir(TargetDirectory):	# Go through every .psd in folder
-		if item.endswith('.psd'):
+		if item.endswith(AcceptedFiles):
+			# Open the image and create a dict
+			OpenedImage = PIL.Image.open(TargetDirectory + "\\" + item)
+			ExifDict = OpenedImage._getexif()
+			
 			# Read the exif data and pick out these things:
 			# Filename, Camera Model, Focal Length, Aperture Size
 			FileName = os.path.basename(item)
-			CameraModel = TAGS[0xc614]
-			FocalLength = TAGS[0x920a]
-			ApertureSize = TAGS[0x9202]
+			CameraModel = ExifDict.get(0x0110)
+			FocalLengthRaw = ExifDict.get(0x920a)
+			ApertureSizeRaw = ExifDict.get(0x829d)
+			
+			# Convert to str and get the important strings
+			FocalLength = str(FocalLengthRaw)[1:3] + "mm"
+			ApertureSize = "f/" + str(ApertureSizeRaw)[1:4] 
 			
 			print(FileName)
 			print(CameraModel)
 			print(FocalLength)
 			print(ApertureSize)
+			print("")
 			
 			csvOutput.writerow({'Filename': FileName, 'Camera Model': CameraModel, 'Focal length': FocalLength, 'Aperture size': ApertureSize})
