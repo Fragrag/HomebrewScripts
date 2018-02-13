43b3b7dbdff59f0527d2490568c389d768fbdec0
Fragrag <haryo.sukmawanto@gmail.com>
2018-02-13 10:03:49 +0100

    -Cleaned and optimised ImageExifData, removed returning aperture value as it is not required for the project.

--- confetti/ImageExifData.py
+++ confetti/ImageExifData.py
@@ -1,12 +1,13 @@
-# https://stackoverflow.com/questions/4764932/in-python-how-do-i-read-the-exif-data-for-an-image
-# https://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/EXIF.html
-# Filename = 
-# Camera model = 0xc614
-# Focal length = 0x920a
-# Aperture = 0x9202
-
-# Dependencies
-# csv, os, Pillow, sys
+##########################################################################
+#                                                                        #
+# ImageExifData.py                                                       #
+#                                                                        #
+# This script goes through images in a folder	          				 #
+# and returns the file's location, model and focal length to output.csv  #      
+#                                                                        #
+##########################################################################
+# EXIF Tags reference: https://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/EXIF.html
+# Dependencies: csv, os, sys, Pillow
 
 import csv
 import os
@@ -22,34 +23,33 @@ except:
 	
 AcceptedFiles = '.psd', '.cr2', '.jpg', '.jpeg', '.dng'
 
-
-with open('output.csv', 'w') as csvfile: 		# Initialize and create new .csv
-	header = ['Filename', 'Camera Model', 'Focal length', 'Aperture size']
+# Initialize and create new .csv
+with open('output.csv', 'w') as csvfile: 		
+	header = ['Filename', 'Camera Model', 'Focal length']
 	csvOutput = csv.DictWriter(csvfile, fieldnames=header, lineterminator='\n', delimiter=',')
 	csvOutput.writeheader()
 	print("")
 	
-	for item in os.listdir(TargetDirectory):	# Go through every .psd in folder
+	for item in os.listdir(TargetDirectory):
 		if item.endswith(AcceptedFiles):
-			# Open the image and create a dict
+			# Open the image and create a dict from the exif
 			OpenedImage = PIL.Image.open(TargetDirectory + "\\" + item)
 			ExifDict = OpenedImage._getexif()
 			
 			# Read the exif data and pick out these things:
-			# Filename, Camera Model, Focal Length, Aperture Size
+			# Filename, Camera Model, Focal Length
 			FileName = os.path.basename(item)
 			CameraModel = ExifDict.get(0x0110)
 			FocalLengthRaw = ExifDict.get(0x920a)
-			ApertureSizeRaw = ExifDict.get(0x829d)
 			
-			# Convert to str and get the important strings
-			FocalLength = str(FocalLengthRaw)[1:3] + "mm"
-			ApertureSize = "f/" + str(ApertureSizeRaw)[1:4] 
+			# Convert raw values to string and get the important characters
+			FocalLength = "{0[0]}".format(FocalLengthRaw) + "mm"
 			
+			# Print output for debugging
 			print(FileName)
 			print(CameraModel)
 			print(FocalLength)
-			print(ApertureSize)
 			print("")
 			
-			csvOutput.writerow({'Filename': FileName, 'Camera Model': CameraModel, 'Focal length': FocalLength, 'Aperture size': ApertureSize})
+			#Write the row in the csv
+			csvOutput.writerow({'Filename': FileName, 'Camera Model': CameraModel, 'Focal length': FocalLength})
