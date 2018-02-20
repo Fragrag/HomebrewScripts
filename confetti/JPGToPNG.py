7e677d7723e22e051ff5f29236c68c4ee70bd84c
Fragrag <haryo.sukmawanto@gmail.com>
2018-02-20 17:59:09 +0100

    Initial commit of JPGToPNG.py

---
+++ confetti/JPGToPNG.py
@@ -0,0 +1,21 @@
+import csv
+import os
+import sys
+import PIL.Image
+
+try:
+	InputDirectory = sys.argv[1]
+	assert InputDirectory
+except:
+	print("Please specify an input folder")
+	sys.exit(1)
+
+AcceptedFiles = '.jpg', '.jpeg'
+
+for item in os.listdir(InputDirectory):
+	if item.endswith(AcceptedFiles):
+
+		OpenedImage = PIL.Image.open(InputDirectory + "\\" + item)
+		OpenedImageFilename, OpenedImageFileExtension = os.path.splitext(InputDirectory + "\\" + item)
+		OpenedImage.save(OpenedImageFilename + ".png"[, 'png'[, optimize]])
+		OpenedImage.close()
