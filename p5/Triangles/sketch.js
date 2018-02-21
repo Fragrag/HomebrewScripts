73df94231781a44f1a951f64a70b794ac0f64c9c
Fragrag <haryo.sukmawanto@gmail.com>
2018-02-21 23:14:13 +0100

    p5/triangles
    Oh dear why is this so trippy

--- p5/Triangles/sketch.js
+++ p5/Triangles/sketch.js
@@ -1,3 +1,5 @@
+let triangles = [];
+
 function setup() {
 	createCanvas(400, 300);
   
@@ -6,28 +8,8 @@ function setup() {
 function draw() {
 	// arc(200, 150, 75, 75, 0, 90);
 	background(192);
-	DrawTriangle(200, 150, 75);
-	ellipse(200, 150, 75, 75);
-	
-	
-}
 
-function DrawTriangle(px, py, r)
-{
-	OriginX = px;
-	OriginY = py;
-	Radius = r;
-	Xoffset = cos(PI) * Radius;
-	Yoffset = sin(PI) * Radius;
-	
-	x1 = OriginX;
-	y1 = OriginY - r;
-	
-	x2 = OriginX + Xoffset;
-	y2 = OriginY + Yoffset;
-
-	x3 = OriginX - Xoffset;
-	y3 = OriginY - Yoffset;
-	
-	triangle(x1, y1, x2, y2, x3, y3);
+	for (let x = 0; x < PI; x += PI/10){
+		triangles.push(new equilateralTriangle(200, 150, 150, x));
+	}
 }
