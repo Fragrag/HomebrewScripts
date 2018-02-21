3332a675f8a35176951758e5c039d0f1525f4597
Fragrag <haryo.sukmawanto@gmail.com>
2018-02-21 17:34:02 +0100

    -How on earth is making an equilateral triangle so difficult

--- p5/Triangles/sketch.js
+++ p5/Triangles/sketch.js
@@ -1,24 +1,33 @@
 function setup() {
-  createCanvas(400, 300);
+	createCanvas(400, 300);
+  
 }
 
 function draw() {
 	// arc(200, 150, 75, 75, 0, 90);
-	Drawtriangle(200, 150, 75);
+	background(192);
+	DrawTriangle(200, 150, 75);
+	ellipse(200, 150, 75, 75);
+	
 	
 }
 
-function Drawtriangle(px, py, r)
+function DrawTriangle(px, py, r)
 {
-	circleOriginX = px;
-	circleOriginY = py;
-	circleRadius = r;
+	OriginX = px;
+	OriginY = py;
+	Radius = r;
+	Xoffset = cos(PI) * Radius;
+	Yoffset = sin(PI) * Radius;
 	
-	x2 = (circleRadius * cos(120 * PI / 180)) + circleOriginX;
-	y2 = (circleRadius * cos(120 * PI / 180)) + circleOriginY;
+	x1 = OriginX;
+	y1 = OriginY - r;
+	
+	x2 = OriginX + Xoffset;
+	y2 = OriginY + Yoffset;
 
-	x3 = (circleRadius * cos(240 * PI / 180)) + circleOriginX;
-	y3 = (circleRadius * cos(240 * PI / 180)) + circleOriginY;
+	x3 = OriginX - Xoffset;
+	y3 = OriginY - Yoffset;
 	
-	triangle(px, py+r, x2, y2, x3, y3);
+	triangle(x1, y1, x2, y2, x3, y3);
 }
--- p5/Triangles/triangles.js
+++ p5/Triangles/triangles.js
@@ -6,11 +6,11 @@ class equilateralTriangle
 		this.circleOriginY = py;
 		this.circleRadius = r;
 		
-		float x2 = (circleRadius * Math.Cos(120 * PI / 180)) + circleOriginX;
-		float y2 = (circleRadius * Math.Cos(120 * PI / 180)) + circleOriginY;
+		float x2 = (circleRadius * cos(120 * PI / 180)) + circleOriginX;
+		float y2 = (circleRadius * cos(120 * PI / 180)) + circleOriginY;
 
-		float x3 = (circleRadius * Math.Cos(240 * PI / 180)) + circleOriginX;
-		float y3 = (circleRadius * Math.Cos(240 * PI / 180)) + circleOriginY;
+		float x3 = (circleRadius * cos(240 * PI / 180)) + circleOriginX;
+		float y3 = (circleRadius * cos(240 * PI / 180)) + circleOriginY;
 		
 		this.triangle(px, py+r, x2, y2, x3, y3);
 	}
