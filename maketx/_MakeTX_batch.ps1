a52bb492485d9c92a65b8ba1bc6c793351274cb2
Fragrag <haryo.sukmawanto@gmail.com>
2019-07-08 17:53:28 +0200

    Commiting train work

--- maketx/_MakeTX_batch.ps1
+++ maketx/_MakeTX_batch.ps1
@@ -3,14 +3,21 @@
 Get-ChildItem -Recurse $PSScriptRoot |
 ForEach-Object {
     $file = $_.FullName
-    $file_tx = ($_.BaseName + '.tx')
+    $file_tx = ($_.DirectoryName + '\\' + $_.BaseName + '.tx')
     $file_d = (Get-ItemProperty -Path $file -Name LastWriteTime).lastwritetime
     $file_tx_d = (Get-ItemProperty -Path $file_tx -Name LastWriteTime).lastwritetime
+    # Write-Output $file
+    # Write-Output $file_tx
+    # Write-Output $file_d
+    # Write-Output $file_tx_d
+
 
     if (Test-Path $file_tx) {
 
         if ($file_writetime -gt $file_tx_writetime) {
-            Write-Output "Tx file was found and is older than the non-txt file!"
+            # & "C:\ProgramData\Autodesk\ApplicationPlugins\MAXtoA\maketx.exe -v -u --oiio $original -o $txfile"
+            & "C:\solidangle\mtoadeploy\2017\bin\maketx.exe $original -o $txfile"
+
         }
         else {
             Write-Output "Tx file was found and is newer than the non-tx file!"
@@ -18,6 +25,8 @@ ForEach-Object {
     }
 
     else {
-    Write-Output "No tx file was found!"
+        # & "C:\ProgramData\Autodesk\ApplicationPlugins\MAXtoA\maketx.exe -v -u --oiio $original -o $txfile"
+        & "C:\solidangle\mtoadeploy\2017\bin\maketx.exe $original -o $txfile"
+
     }
 }
