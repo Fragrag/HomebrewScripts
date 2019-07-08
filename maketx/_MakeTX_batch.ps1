7dd57d886d127bb97291b2ca580d26087403223a
Fragrag <haryo.sukmawanto@gmail.com>
2019-07-08 23:15:54 +0200

    maketx batch script now functional

--- maketx/_MakeTX_batch.ps1
+++ maketx/_MakeTX_batch.ps1
@@ -3,20 +3,18 @@
 Get-ChildItem -Recurse $PSScriptRoot |
 ForEach-Object {
     $file = $_.FullName
-    $file_tx = ($_.DirectoryName + '\\' + $_.BaseName + '.tx')
-    $file_d = (Get-ItemProperty -Path $file -Name LastWriteTime).lastwritetime
-    $file_tx_d = (Get-ItemProperty -Path $file_tx -Name LastWriteTime).lastwritetime
-    # Write-Output $file
-    # Write-Output $file_tx
-    # Write-Output $file_d
-    # Write-Output $file_tx_d
-
+    $file_tx = ($_.DirectoryName + '\' + $_.BaseName + '.tx')
+    $arg1 = '-v'
+    $arg2 = '-u'
+    $arg3 = '--oiio'
+    $arg4 = '-o'        
+    $cmd = "C:\solidangle\mtoadeploy\2017\bin\maketx.exe"
 
     if (Test-Path $file_tx) {
-
-        if ($file_writetime -gt $file_tx_writetime) {
-            # & "C:\ProgramData\Autodesk\ApplicationPlugins\MAXtoA\maketx.exe -v -u --oiio $original -o $txfile"
-            & "C:\solidangle\mtoadeploy\2017\bin\maketx.exe $original -o $txfile"
+    $file_d = (Get-ItemProperty -Path $file -Name LastWriteTime).lastwritetime
+    $file_tx_d = (Get-ItemProperty -Path $file_tx -Name LastWriteTime).lastwritetime
+        if ($file_d -gt $file_tx_d) {
+            & $cmd $arg1 $arg2 $arg3 $file $arg4 $file_tx
 
         }
         else {
@@ -25,8 +23,7 @@ ForEach-Object {
     }
 
     else {
-        # & "C:\ProgramData\Autodesk\ApplicationPlugins\MAXtoA\maketx.exe -v -u --oiio $original -o $txfile"
-        & "C:\solidangle\mtoadeploy\2017\bin\maketx.exe $original -o $txfile"
+        & $cmd $arg1 $arg2 $arg3 $file $arg4 $file_tx
 
     }
 }
