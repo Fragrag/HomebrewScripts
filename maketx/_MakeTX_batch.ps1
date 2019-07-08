6d82c07d579c095922e2edce18cb385e834242b9
Fragrag <haryo.sukmawanto@gmail.com>
2019-07-08 09:52:19 +0200

    Converting batch converter to powershell

---
+++ maketx/_MakeTX_batch.ps1
@@ -0,0 +1,23 @@
+﻿Write-Output $PSScriptRoot
+
+Get-ChildItem -Recurse $PSScriptRoot |
+ForEach-Object {
+    $file = $_.FullName
+    $file_tx = ($_.BaseName + '.tx')
+    $file_d = (Get-ItemProperty -Path $file -Name LastWriteTime).lastwritetime
+    $file_tx_d = (Get-ItemProperty -Path $file_tx -Name LastWriteTime).lastwritetime
+
+    if (Test-Path $file_tx) {
+
+        if ($file_writetime -gt $file_tx_writetime) {
+            Write-Output "Tx file was found and is older than the non-txt file!"
+        }
+        else {
+            Write-Output "Tx file was found and is newer than the non-tx file!"
+        }
+    }
+
+    else {
+    Write-Output "No tx file was found!"
+    }
+}
